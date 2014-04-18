a87e1474e8f89db1f96748d97717096a96b02c5b
Ehtisham Siddiqui <ehtishams@gmail.com>
2014-04-19 00:39:53 +0500

    Fixed issue#1, captured image forwarding

--- Resources/app.js
+++ Resources/app.js
@@ -1,74 +1,106 @@
 
-
-var win1 = Titanium.UI.createWindow(
-{  
-    title:'NoKunda!',
-    backgroundColor:'#191919'
-});
-var myview = Ti.UI.createView(
-{
-	layout: 'vertical',
-	top : '8%',
-	bottom:'8%',
-	left:'4%',
-	right:'4%',
-	backgroundColor : 'black'
+var win = Titanium.UI.createWindow({
+	title:"Camera Preview",
+	backgroundColor:'#FFFFFF',
+	exitOnClose: true
 });
 
-var coordss = Titanium.UI.createLabel(
-{
-	font:{fontSize:14,fontFamily:'Helvetica Neue'},
-	//textAlign:'center',
-	width:'auto'
-});
+	Titanium.Media.showCamera(
+		{	
+			success:function(e)
+			{
+				//img = e.media;
+				img = e.media.imageAsResized(1024, 1024);
+				var f = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory,'temp.jpg');
+				f.write(img);
+				theimg = f.nativePath;
+				
+					var imView = Titanium.UI.createImageView(
+						{
+						image:theimg,
+						width:290,
+						height:220,
+						top:15,
+						zIndex:1
+					});
+					
+					win.add(imView);
+					
+					var button = Titanium.UI.createButton(
+					{
+						title:"Next...(GPS)",
+						width:100,
+						height:100,
+						bottom:15,
+						zIndex:2
+					});
+					button.addEventListener ("click", function(e)
+					{
+						var gpsWindow = Ti.UI.createWindow(
+							{
+								url: "gps.js",
+								title:'NoKunda Getting GPS!',
+    							backgroundColor:'#191919'
+							});
+							gpsWindow.open();
+					});
+					
+					win.add(button);
+					
+					win.open();
+				
+			},
 
-myview.add(coordss);
-
-win1.add(myview);
-
-win1.open();
-    
-if (Ti.Platform.osname == "android") 
-    {
-    	var providerGps = Ti.Geolocation.Android.createLocationProvider(
-    	{
-	    name: Ti.Geolocation.PROVIDER_GPS,
-	    minUpdateDistance: 0.4,
-	    minUpdateTime: 1
+			error:function(e)
+			{
+				alert("There was an error");
+			},
+			cancel:function(e)
+			{
+				alert("The event was cancelled");
+			},
+			//allowEditing:true,
+			saveToPhotoGallery:true,
+			mediaTypes:[Titanium.Media.MEDIA_TYPE_PHOTO],
+			showControls: true
 		});
-		
-		Ti.Geolocation.Android.addLocationProvider(providerGps);
-		Ti.Geolocation.Android.manualMode = true;
-	}
-else
-	{    
-    Ti.Geolocation.purpose = 'Get Current Location';
-    Ti.Geolocation.distanceFilter = 1;
-	Ti.Geolocation.accuracy = Ti.Geolocation.ACCURACY_BEST;
-    Ti.Geolocation.preferredProvider = Ti.Geolocation.PROVIDER_GPS;
-    //Ti.Geolocation.accuracy = Titanium.Geolocation.ACCURACY_HIGH;
-	}
 
-    Ti.Geolocation.addEventListener('location', function(e)
-    {
-       if (!e.success || e.error)
-       {
-       	  coordss.text = 'Coordinates N/A right now... wait?';
-//          alert('error ' + JSON.stringify(e.error));
-			return;
-       } 
-       
-       coordss.text = 'Lat: ' + e.coords.latitude + ' Long: ' + e.coords.longitude + ' Accu: ' + e.coords.accuracy + '\n Heading: ' + e.coords.heading + ' Speed: ' + e.coords.speed;
-       
-    });
+
+
 
 /*
-if ( isLocationAuthorized() ) 
+var winone = Ti.UI.createWindow(
+	{
+		title : 'MAIN APP.JS',
+		backgroundColor : 'blue',
+		exitOnClose : true
+	}
+);
+
+var btn = Ti.UI.createButton(
+	{
+		title : 'Goto Cam',
+		width:200,
+		height:100,
+		bottom:50
+	}
+);
+
+btn.addEventListener('click', function(e)
 {
-  getLocation();
-} 
-else 
-{
-	alert('Cannot use GPS! :/');
-};
+	 	var winmain = Titanium.UI.createWindow(
+	 	{
+		url : 'cam.js',
+		//title:"Camera Preview",
+		//backgroundColor:'#FFFFFF',
+		//exitOnClose: true
+		});
+	
+	winmain.open();
+}
+);
+
+winone.add(btn);
+winone.open();
+
 */
